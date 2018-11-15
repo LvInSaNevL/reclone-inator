088a2e50a289b82dc708b5c9d83fb371428d3301
LvInSaNevL <mattwollam@gmail.com>
2018-11-15 00:36:40 -0500

    Now checks for internet connection before continuing and file paths are user and location agnostic

---
+++ AutoHotKey.ahk
@@ -0,0 +1,61 @@
+﻿#NoEnv  ; Recommended for performance and compatibility with future AutoHotkey releases.
+; #Warn  ; Enable warnings to assist with detecting common errors.
+SendMode Input  ; Recommended for new scripts due to its superior speed and reliability.
+SetWorkingDir %A_ScriptDir%  ; Ensures a consistent starting directory.
+
+
+; Closes AHK Script
+<^#esc::ExitApp
+
+
+; Display all key codes
+<^#LShift::
+HelpArr := ["Ctrl, Win, Esc: Closes current AHK script"
+		,"Ctrl, Win, Shift: Displays this message"
+		,"Ctrl, Win, Space: Google searches highlighted text"
+		,"Ctrl, Win, Q: Autocompletes link"]
+Loop % HelpArr.MaxIndex()
+	Content .= HelpArr[A_Index] "`n"
+Msgbox % Content
+return
+
+; Ctrl + Space google search copied value when no full screen apps
+<^#space::
+Gosub Check_FullScreenActive
+If !(FullScreenActive) {
+	Send,^c
+	ClipWait
+	Run, http://www.google.com/search?q=%Clipboard%
+}	
+return
+
+; Autocompletes Link
+<^#Q::
+Gosub Check_FullScreenActive
+If !(FullScreenActive) {
+	Send, ^x
+	LinkString = https://www.%Clipboard%.com
+	ClipWait
+	SendInput % LinkString
+}
+return
+
+; Global Media Keys
+
+
+; Check if full screen
+Check_FullScreenActive:
+wingetpos,,,w,h,A
+wwwh=%w%%h%
+swsh=%a_screenwidth%%a_screenheight%
+WinGet, Style, Style, A
+FullScreenActive := false
+if !(Style & 0xC00000) {
+	if(wwwh = swsh) {
+		FullScreenActive := true
+	}
+}
+return
+
+; Autocompletion ( https://github.com/dwyl/english-words )
+String := ""
--- newsystem.ps1
+++ newsystem.ps1
@@ -26,7 +26,25 @@ $software = @(("https://discordapp.com/api/download/canary?platform=win", "Disco
               ("https://cdn-fastly.obsproject.com/downloads/OBS-Studio-22.0.2-Full-Installer-x64.exe", "OBS", "obs"),
               ("https://download.microsoft.com/download/1/3/F/13F19BF0-17CF-4D0F-938C-41D0489C3FE6/KB3133719-x64.msu.msu", "Microsoft Media Pack", "microsoft_media_pack"))
 
+# Global Variables              
 $start_time = Get-Date            
+$prog_loc = (Resolve-Path .\).Path
+$user = $env:username
+
+# Checks internet connection
+$connected = $false
+while (!($connected))
+{
+    if (test-connection 8.8.8.8 -Count 1 -Quiet) {
+        $connected = $true
+        Write-Host "Connected to the internet, continuing"
+    }
+    else 
+    { 
+        Write-Host "Please connect to the internet" 
+        Start-Sleep -m 2500
+    }
+}
 
 # Sets permissions and methods for the rest of the scripts
 Invoke-Expression "& Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass"
@@ -36,14 +54,14 @@ Invoke-Expression "& Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass"
 Invoke-Expression "& cd 'HKCU:\Control Panel\Desktop'"
 Invoke-Expression "& Set-ItemProperty -Path . -Name LogPixels -Value 125"
 
+
 # Installs Chocolatey if it's not already installed              
 If(-Not (Test-Path -Path "$env:ProgramData\Chocolatey")) {
     Invoke-Expression "& ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
 }
 
 # Install .NET CLI environment
-#Invoke-Expression "& .\dotnet-install.ps1"
-
+Invoke-Expression "& .\dotnet-install.ps1"
 
 # Installs each package in Chocolatey
 Foreach ($p in $packages)
@@ -53,14 +71,16 @@ Foreach ($p in $packages)
     Invoke-Expression "& $command"
 }
 
-# Installs AHK Scripts
+# Runs AHK Script
+Write-Host "Starting AutoHotKey script"
+Invoke-Expression "& .\AutoHotKey.ahk"
 
 # Installs the rest of the software
 Foreach ($s in $software)
 {
     $install_start_time = Get-Date
     $filename = $s[2]
-    $output = "C:\Users\mwollam\Downloads\$filename.exe"
+    $output = "C:\Users\$user\Downloads\$filename.exe"
 
     Write-Host "Downloading" $s[1]
     Invoke-WebRequest -Uri $s[0] -OutFile $output
@@ -72,5 +92,6 @@ Foreach ($s in $software)
 }
 
 # Finally, restarts the computer so changes take effect
-Write-Output "Time taken: $((Get-Date).Subtract($install_start_time).Seconds) second(s)"
+Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+Write-Host "Restarting computer to save changes"
 #Restart-Computer    
